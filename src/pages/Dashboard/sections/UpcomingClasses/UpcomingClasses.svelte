1fcd3de3238181f10fc3467983c6e29b5920ec23
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-07 03:58:50 +0800

    Add schedule button

--- src/pages/Dashboard/sections/UpcomingClasses/UpcomingClasses.svelte
+++ src/pages/Dashboard/sections/UpcomingClasses/UpcomingClasses.svelte
@@ -1,9 +1,26 @@
 <script>
+  import { Button, Icon } from 'svelte-materialify';
   import Card from '../../../../Components/Card/Card.svelte';
+  import { mdiArrowRight } from '@mdi/js';
 </script>
 
 <Card>
   <svelte:fragment slot="card_title">Upcoming Classes</svelte:fragment>
   <svelte:fragment slot="card_body">Body elements go here...</svelte:fragment>
-  <svelte:fragment slot="card_footer">Buttons go here...</svelte:fragment>
+  <svelte:fragment slot="card_footer">
+    <div class="button_wrapper">
+      <Button text>
+        Weekly Schedule
+        <Icon path={mdiArrowRight} class="ml-1" />
+      </Button>
+    </div>
+  </svelte:fragment>
 </Card>
+
+<style lang="scss">
+  .button_wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+</style>
