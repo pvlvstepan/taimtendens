c7d9e277dc7d6f3cef3ea79d99eff4753d502f2e
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-07 04:41:55 +0800

    Add ListItem component

--- src/pages/Dashboard/sections/UpcomingClasses/UpcomingClasses.svelte
+++ src/pages/Dashboard/sections/UpcomingClasses/UpcomingClasses.svelte
@@ -1,12 +1,42 @@
 <script>
-  import { Button, Icon } from 'svelte-materialify';
+  import ListItem from '../../../../Components/ListItem/ListItem.svelte';
   import Card from '../../../../Components/Card/Card.svelte';
-  import { mdiArrowRight } from '@mdi/js';
+  import { Button, Icon } from 'svelte-materialify';
+  import { mdiArrowRight, mdiEmoticon } from '@mdi/js';
 </script>
 
 <Card>
   <svelte:fragment slot="card_title">Upcoming Classes</svelte:fragment>
-  <svelte:fragment slot="card_body">Body elements go here...</svelte:fragment>
+  <svelte:fragment slot="card_body">
+    <ListItem>
+      <svelte:fragment slot="left_element">
+        <Icon path={mdiEmoticon} />
+      </svelte:fragment>
+      <svelte:fragment slot="body_title">First List Item Title</svelte:fragment>
+      <svelte:fragment slot="body_content">Some text here</svelte:fragment>
+      <svelte:fragment slot="body_alt">
+        There's more space if you need it...
+      </svelte:fragment>
+      <svelte:fragment slot="right_element">
+        <Icon path={mdiEmoticon} />
+      </svelte:fragment>
+    </ListItem>
+    <ListItem>
+      <svelte:fragment slot="left_element">
+        <Icon path={mdiEmoticon} />
+      </svelte:fragment>
+      <svelte:fragment slot="body_title">
+        Second List Item Title
+      </svelte:fragment>
+      <svelte:fragment slot="body_content">Some text here</svelte:fragment>
+      <svelte:fragment slot="body_alt">
+        There's more space if you need it...
+      </svelte:fragment>
+      <svelte:fragment slot="right_element">
+        <Icon path={mdiEmoticon} />
+      </svelte:fragment>
+    </ListItem>
+  </svelte:fragment>
   <svelte:fragment slot="card_footer">
     <div class="button_wrapper">
       <Button text>
