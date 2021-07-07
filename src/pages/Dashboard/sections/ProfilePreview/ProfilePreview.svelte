4f558bb5cdf1ee143dee6e40c3598c6e34a0ae01
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-07 17:53:43 +0800

    Add attendance modal

--- src/pages/Dashboard/sections/ProfilePreview/ProfilePreview.svelte
+++ src/pages/Dashboard/sections/ProfilePreview/ProfilePreview.svelte
@@ -2,6 +2,16 @@
   import Card from '../../../../Components/Card/Card.svelte';
 
   import { Button } from 'svelte-materialify';
+  import AttendanceModal from '../../../../Components/AttendanceModal/AttendanceModal.svelte';
+
+  let active = false;
+
+  function open() {
+    active = true;
+  }
+  function close() {
+    active = false;
+  }
 </script>
 
 <Card>
@@ -9,10 +19,13 @@
     <h5>Joe Mamah</h5>
   </div>
   <div class="button_wrapper">
-    <Button class="primary-color" style="flex: 1">Sign Attendance</Button>
+    <Button class="primary-color" style="flex: 1" on:click={open}
+      >Sign Attendance</Button
+    >
     <Button class="primary-color" style="flex: 1">View Timetable</Button>
   </div>
 </Card>
+<AttendanceModal {active} {close} />
 
 <style lang="scss">
   .button_wrapper {
