d0d00dcf051f2a75e2a04c9a6f0789d72d0ec2ab
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-09 15:16:55 +0900

    Profile Preview Redesign

--- src/pages/Dashboard/sections/ProfilePreview/ProfilePreview.svelte
+++ src/pages/Dashboard/sections/ProfilePreview/ProfilePreview.svelte
@@ -11,24 +11,25 @@
 </script>
 
 <Card>
-  <div class="Avatar">
-    <Avatar size="80px" class="primary-color">JM</Avatar>
-  </div>
-  <div>
-    <h5 class="text-h5 text-center">Joe Mamah</h5>
-  </div>
-
-  <h6 class="text-subtitle-1 text-center text--secondary">
-    TP0XXXXX | UCDF1909ICT(SE)
-  </h6>
-
-  <div class="button_wrapper">
-    <Button icon class="grey-text" size="x-large">
-      <Icon path={mdiAccountCog} size="30px" />
-    </Button>
-    <Button icon class="red-text" size="x-large">
-      <Icon path={mdiExitToApp} size="30px" />
-    </Button>
+  <div class="profile_wrapper">
+    <div class="avatar_wrapper">
+      <Avatar size="120px" class="primary-color text-center text-h2">JM</Avatar>
+    </div>
+    <div class="profile_details">
+      <div class="text-body-1">Welcome Back,</div>
+      <h4 class="text-h4 text-center">Joe Mamah</h4>
+      <h6 class="text-subtitle-1 text-center text--secondary">
+        TP0XXXXX | UCDF1909ICT(SE)
+      </h6>
+    </div>
+    <div class="button_wrapper col">
+      <Button icon class="grey-text" size="x-large">
+        <Icon path={mdiAccountCog} size="30px" />
+      </Button>
+      <Button icon class="red-text" size="x-large">
+        <Icon path={mdiExitToApp} size="30px" />
+      </Button>
+    </div>
   </div>
   <div class="button_wrapper">
     <Button
@@ -51,6 +52,24 @@
 >
 
 <style>
+  .profile_wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    flex-direction: row;
+    column-gap: 30px;
+    padding: 16px;
+    border-radius: 8px;
+    background-color: var(--theme-app-bar);
+  }
+  .profile_details {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    row-gap: 10px;
+    height: 100%;
+    flex: 75%;
+  }
   .button_wrapper {
     width: 100%;
     display: flex;
@@ -59,10 +78,13 @@
     justify-content: center;
     column-gap: 15px;
   }
-
-  .Avatar {
-    font-size: 30pt;
+  .button_wrapper.col {
+    flex-direction: column;
+    row-gap: 15px;
+    flex: 15%;
+  }
+  .avatar_wrapper {
     align-items: center;
-    text-align: center;
+    flex: auto;
   }
 </style>
