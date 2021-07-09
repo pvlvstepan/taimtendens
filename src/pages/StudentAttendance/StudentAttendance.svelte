227d678c5dad6d9add48b88337488653e7e641bc
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-07-10 02:10:45 +0900

    Add intake selector

--- src/pages/StudentAttendance/StudentAttendance.svelte
+++ src/pages/StudentAttendance/StudentAttendance.svelte
@@ -3,9 +3,13 @@
   import AttendaceSummary from './sections/AttendanceSummary/AttendanceSummary.svelte';
   import SemesterAttendance from './sections/SemesterAttendance/SemesterAttendance.svelte';
   import AttendanceLegends from './sections/AttendanceLegends/AttendanceLegends.svelte';
+  import IntakeSelector from './sections/IntakeSelector/IntakeSelector.svelte';
+
+  const intakes = ['UCDF1909ICT(SE)', 'UCDF2009ICT(SE)'];
 </script>
 
 <CardWrapper>
+  <IntakeSelector {intakes} />
   <AttendaceSummary />
   <SemesterAttendance semesterID={1} />
   <SemesterAttendance semesterID={2} />
